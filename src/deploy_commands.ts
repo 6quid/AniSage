e32cd7e28537aad82832b69ae9ae1f600c3c4fda
squid <anmolca.0125@gmail.com>
2024-11-18 16:25:28 -0500

    Refactor command deployment to register global application commands

--- src/deploy_commands.ts
+++ src/deploy_commands.ts
@@ -46,10 +46,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN!);
 
     //FOR DEV PURPOSE ONLY PRIVATE GUILD
     const data: APIApplicationCommand[] = (await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID!,
-        process.env.GUILD_ID!
-      ),
+      Routes.applicationCommands(process.env.CLIENT_ID!),
       { body: commands }
     )) as APIApplicationCommand[];
 
